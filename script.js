88e3781ffa0ec6e5e4edfc7d88579f030b8ed1d7
rafaeltfreddi-coder <rafaeltfreddi@gmail.com>
2026-09-25 12:21:23 -0300

    ssss

---
+++ script.js
@@ -0,0 +1,14 @@
+function calcularMedia () {
+    const textoNota1 = document.getElementyById("nota1").value
+    const textoNota2 = document.getElementyById("nota2").value
+    const textoNota3 = document.getElementyById("nota3").value
+
+    const nota1 = Number(textoNota1)
+    const nota2 = Number(textoNota2)
+    const nota3 = Number(textoNota3)
+
+    const media = (nota1 + nota2 + nota3) / 3
+    let situacao
+    if (media >=7){}
+
+}
